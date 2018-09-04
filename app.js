b8b5e19a39b5a473089665c225812f8ffb2e0e03
Jin Park <jinpark1dev@gmail.com>
2018-09-03 17:19:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+// string
+var myName = 'Jin';
+// myName = 23;
+// number
+var myAge = 27.5;
+// myAge = 'Jin';
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27';
+// array
+var hobbies = ["Cooking", "Sports"];
+console.log(typeof hobbies);
+hobbies = [100];
+// tuples
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    console.log("Hello!");
+}
+// argument types 
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(10, 2));
+// function types 
+var myMultiply;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+// objects
+var userData = {
+    name: "Jin",
+    age: 27
+};
+// userData = {
+//     a: "Hello",
+//     b: 22
+// };
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types
+var myRealRealAge = 27;
+myRealRealAge = "27";
+// check types
+var finalValue = "A string";
+if (typeof finalValue == "string") {
+    console.log("Final value is a number");
+}
+// never
+function neverReturns() {
+    throw new Error('An error!');
+}
+// Nullable Types
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull = null;
+canAlsoBeNull = null;
+var canThisBeAny = null;
+canThisBeAny = 12;
+// Arrow Functions
+var multiplyNumbers = function (number1, number2) { return number1 * number2; };
+console.log(multiplyNumbers(10, 3));
+var great = function () {
+    console.log("Hello");
+};
+great();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend("Hal");
---
+++ app.ts
@@ -0,0 +1,131 @@
+// string
+let myName: string = 'Jin';
+// myName = 23;
+
+// number
+let myAge: number = 27.5;
+// myAge = 'Jin';
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"];
+console.log(typeof hobbies);
+hobbies = [100];
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Gray, // 0
+    Green = 100, // 1
+    Blue // 2
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor)
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3};
+console.log(car);
+
+// functions
+function returnMyName(): string {
+    return myName;
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+    console.log("Hello!");
+}
+
+// argument types 
+function multiply(value1: number, value2: number): number {
+    return value1 * value2
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(10, 2));
+
+// function types 
+let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5,2));
+
+// objects
+let userData: { name: string, age: number } = {
+    name: "Jin",
+    age: 27
+};
+// userData = {
+//     a: "Hello",
+//     b: 22
+// };
+
+// complex object
+let complex: {data: number[], output: (all: boolean) => number[]} = {
+    data: [100, 3.99, 10],
+
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+};
+// complex = {};
+
+// type alias
+type Complex = {data: number[], output: (all: boolean) => number[]};
+
+let complex2: Complex = {
+    data: [100, 3.99, 10],
+
+    output: function (all: boolean): number[] {
+        return this.data;
+    }
+};
+
+// union types
+let myRealRealAge: number | string = 27;
+myRealRealAge = "27";
+
+// check types
+let finalValue = "A string";
+if (typeof finalValue == "string") {
+    console.log("Final value is a number");
+}
+
+// never
+function neverReturns():never {
+    throw new Error('An error!');
+}
+
+// Nullable Types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull = null;
+canAlsoBeNull = null;   
+let canThisBeAny: number | null = null;
+canThisBeAny = 12;
+
+// Arrow Functions
+const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
+console.log(multiplyNumbers(10, 3))
+
+const great = () => {
+    console.log("Hello");
+};
+great();
+
+const greetFriend = friend => console.log(friend);
+greetFriend("Hal");
